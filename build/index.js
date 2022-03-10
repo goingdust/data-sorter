0e95712dcc536469f6f028eb78ea03ae4bbaa4fa
goingdust <dustinluck@gmail.com>
2022-03-10 13:16:25 -0500

    Create CharactersCollection to work Sorter with strings

--- build/index.js
+++ build/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 10]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Treehouse');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 10]);
-const sorter = new Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection('Treehouse');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
