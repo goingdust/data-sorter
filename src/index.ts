cabaf0f25136c0fd305d5ffaba675dbca26e80d0
goingdust <dustinluck@gmail.com>
2022-03-10 19:42:11 -0500

    Make Sorter abstract class and parent of others then call sort on each

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 10]);
-// const charactersCollection = new CharactersCollection('Treehouse');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 10]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Treehouse');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -17,6 +18,5 @@ linkedList.add(4);
 linkedList.add(37);
 linkedList.add(3);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
